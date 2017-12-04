624e46fc0e5a55068b5b5fb854329fe3af8c9a7a
djsulli24 <djsulli24@gmail.com>
2017-12-04 10:41:54 -0600

    Toggling functionality between animated and still has been added for click of images.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
                 }).done(function(response) {
                     // Empty out the gifs already on the page before adding new ones
                     $("#gifcontainer").empty();
-                    response.data.map(function(value) {
+                    response.data.forEach(function(value, index) {
                         $("#gifcontainer").append(
                             `<p>Rating: ` + value.rating + `</p>
-                            <img id='gifinsert' 
-                            class='img-responsive'
-                            data-animage='` + value.images.fixed_width.url + `'
+                            <img
+                            class='img-responsive gifinsert'
+                            data-animate='` + value.images.fixed_width.url + `'
                             data-still='` + value.images.fixed_width_still.url + `'
                             status='still' 
                             src='` + value.images.fixed_width_still.url + `'/>`
@@ -25,15 +25,29 @@ $(document).ready(function() {
                 }
             );
         },
-
+        toggle: function(image) {
+            if (image.attr("status") === "still") {
+                image.attr("src", image.attr("data-animate"));
+                image.attr("status", "animated");
+            }
+            else {
+                image.attr("src", image.attr("data-still"));
+                image.attr("status", "still");
+            }
+        },
     };
 
     // ------------BUTTON CLICK EVENTS--------------
 
+    // Click a button to add gifs to the page
     $("#buttoncontainer").on("click", ".gifbutton", function(event) {
         event.preventDefault();
-
         giftastic.giphyAPI($(this).attr("search-term"));
-    } );
+    });
+
+    // Click a gif to toggle between animated and still
+    $("#gifcontainer").on("click", ".gifinsert", function() {
+        giftastic.toggle($(this));
+    });
 
 });
